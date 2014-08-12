3ef310702f45ab6b027527a196d6ec934068fa71
nirvanbd <nirvanbd@yahoo.com>
2014-08-12 11:33:38 +0530

    Form Runtime Controller Missed out in previous commit

---
+++ BS_FormBuilder.Web/Controllers/FormRuntimeController.cs
@@ -0,0 +1,35 @@
+﻿using BS_FormBuilder.Web.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BS_FormBuilder.Web.Controllers
+{
+    public class FormRuntimeController : Controller
+    {
+        private FormBuilderContext db = new FormBuilderContext();
+
+        // GET: /FormBuilder/Edit/5
+        public ActionResult Edit(int? id) {
+            ViewBag.EditMode = "edit";
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Form form = db.Forms.Find(id);
+            if (form == null) {
+                return HttpNotFound();
+            }
+            return View(form);
+        }
+
+        // POST: /FormBuilder/Edit/5  ...ToDo awaiting Arnabs call
+        [HttpPost]
+        public ActionResult Edit(int id) {
+            return RedirectToAction("Edit", new {id = id});
+        }
+
+    }
+}
